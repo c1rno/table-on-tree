d1837767bf0c58bd7c2d1a36c6344d6339556148
c1rno <i32311113@icloud.com>
2017-08-31 08:05:01 +0700

    add index.html and swap normal routes to hash

--- src/bootstrap/syncStore.js
+++ src/bootstrap/syncStore.js
@@ -1,10 +1,10 @@
-import { browserHistory } from 'react-router'
+import { browserHistory, hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 export default async function (store) {
 	return new Promise( (resolve) => {
 		setTimeout( () => {
-			resolve(syncHistoryWithStore(browserHistory, store))
+			resolve(syncHistoryWithStore(hashHistory, store))
 		}, 2000)
 	})
 }
